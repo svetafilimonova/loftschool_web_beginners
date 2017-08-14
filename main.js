55f5b23ac43da8e3891101131924dfaf46a12e87
Svetlana Filimonova <lana.shvedova@gmail.com>
2017-08-14 16:55:03 +0300

    added animation to fullscreen menu

--- main.js
+++ main.js
@@ -1,31 +1,14 @@
 $(document).ready(function(){
 
-$('.ham-button').on('click', function(e){
-    e.preventDefault();
-
-$('.hamburger-menu').css('display', 'block');
-   
-
-})
-
-$('.hamburger-menu__close').on('click', function(e){
-    e.preventDefault();
-
-   $('.hamburger-menu').css('display', 'none'); 
-     
-    
-    })
-
-
     $('.ham-button').on('click', function(e){
     e.preventDefault()
-        $('.hamburger-menu').fadeIn(3000);   
+        $('.hamburger-menu').fadeIn(2000);   
 
 })
 
     $('.hamburger-menu__close').on('click', function(e){
     e.preventDefault()
-        $('.hamburger-menu').fadeOut(3000);  
+        $('.hamburger-menu').fadeOut(2000);  
 
 })
 
